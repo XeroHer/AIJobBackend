a7aebfb0422e0955eb04c6d655d82832dcd933a1
XeroHer <bksraut27@gmail.com>
2026-03-29 22:42:51 +0100

    chnage

--- routes/register.js
+++ routes/register.js
@@ -10,11 +10,16 @@ const router = express.Router();
 
 /* ================== EMAIL TRANSPORT ================== */
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // IMPORTANT
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
 });
 
 /* ================== HELPER FUNCTIONS ================== */
